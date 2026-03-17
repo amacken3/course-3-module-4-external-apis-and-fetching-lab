8b424afec32c504b22e6a8d42cff430a5411d9dc
Aengus MacKenzie <aengus.2000@gmail.com>
2026-03-17 14:25:10 -0400

    fetchWeatherAlerts and displayAlerts setup

--- index.js
+++ index.js
@@ -2,3 +2,26 @@
 const weatherApi = "https://api.weather.gov/alerts/active?area="
 
 // Your code here!
+function fetchWeatherAlerts(state) {
+    fetch(`https://api.weather.gov/alerts/active?area=${state}`)
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        displayAlerts(data);
+    })
+    .catch((error) => {
+        console.log(error.message);
+    })
+}
+
+function displayAlerts(data) {
+    console.log(data.title);
+    console.log(data.features.length);
+
+    data.features.forEach((alert) => {
+        console.log(alert.properties.headline);
+    });
+}
+
+fetchWeatherAlerts("TN");
